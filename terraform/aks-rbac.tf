d9683bd590d86a087291b19e8a62261734bfceea
sj <sebastianthegreatful@gmail.com>
2019-02-01 08:56:20 +0100

    Postfix resource names with terraform workspace name

--- terraform/aks-rbac.tf
+++ terraform/aks-rbac.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "k8s" {
-  name = "${var.resource_group_name}"
+  name = "${var.resource_group_name}-${terraform.workspace}"
   location = "${var.location}"
 }
 
 resource "azurerm_azuread_application" "k8s" {
   # TODO: Use $var.name_prefix for all names
   # TODO: Promote this to a TF_VAR_*
-  name = "AKSAADCluster"
+  name = "AKSAADCluster-${terraform.workspace}"
 }
 
 resource "azurerm_azuread_service_principal" "k8s" {
@@ -25,7 +25,7 @@ resource "azurerm_azuread_service_principal_password" "k8s" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  name = "${var.cluster_name}"
+  name = "${var.cluster_name}-${terraform.workspace}"
   location = "${azurerm_resource_group.k8s.location}"
   resource_group_name = "${azurerm_resource_group.k8s.name}"
   dns_prefix = "${var.dns_prefix}"
@@ -40,7 +40,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 
   agent_pool_profile {
-    name = "default"
+    name = "default-${terraform.workspace}"
     count = "${var.agent_count}"
     vm_size = "${var.vm_size}"
     os_type = "Linux"
@@ -53,7 +53,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 
   tags {
-    Environment = "Development"
+    Environment = "${terraform.workspace}"
   }
 
   role_based_access_control {
