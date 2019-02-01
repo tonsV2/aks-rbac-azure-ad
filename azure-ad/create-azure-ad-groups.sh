7ebfaa7aaefbbb5b42402b90440c89814432b415
sj <sebastianthegreatful@gmail.com>
2019-02-01 13:10:15 +0100

    Fix warnings and add note

--- azure-ad/create-azure-ad-groups.sh
+++ azure-ad/create-azure-ad-groups.sh
@@ -6,12 +6,12 @@ GROUP_NAME=cluster-admin
 MAIL_NICKNAME=snot # TODO: Wtf is this?
 
 CLUSTER_ADMIN_GROUP_OBJECT_ID=$(az ad group show --group ${GROUP_NAME} --query objectId -o tsv)
-if test -z ${CLUSTER_ADMIN_GROUP_OBJECT_ID}; then
+if test -z "$CLUSTER_ADMIN_GROUP_OBJECT_ID"; then
   CLUSTER_ADMIN_GROUP_OBJECT_ID=$(az ad group create --display-name ${GROUP_NAME} --mail-nickname ${MAIL_NICKNAME} --query objectId -o tsv)
 fi
 
 # TODO: Turn this into a helm chart with a value for $CLUSTER_ADMIN_GROUP_OBJECT_ID (and metadata.name and possible others)
-# In fact delete the folder k8s-rbac/ and don't ever use kubectl apply
+# In fact delete the folder k8s-rbac/ and don't every use kubectl apply
 echo "apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
