50d9615c96b0f7b231ad333877da2f47186dfcde
erik <erik@erik-Lenovo-V330-14IKB>
2019-04-09 17:14:13 +0200

    Accept labels as-is

--- terraform/aks-rbac.tf
+++ terraform/aks-rbac.tf
@@ -1,6 +1,3 @@
-#
-# Shouldn't resources have a label that indicates environment type instead of just "k8s" ?
-#
 resource "azurerm_resource_group" "k8s" {
   name = "${var.resource_group_name}-${terraform.workspace}"
   location = "${var.location}"
